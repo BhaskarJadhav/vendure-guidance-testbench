44ca67ae6e3b2eb53b9be83688fef47a658d018a
Jadhav Bhaskar <bhaskarjadhav9666@email.com>
2026-06-18 02:44:53 +0530

    Configure integrated JobQueueService in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import { bootstrap } from '@vendure/core';
+import { bootstrap, JobQueueService } from '@vendure/core';
 import { config } from './vendure-config';
 
 bootstrap(config)
-  .then(() => {
+  .then(async (app) => {
     console.log('--------------------------------------------------');
     console.log('Vendure Shop API running on: http://localhost:3000/shop-api');
     console.log('Vendure Admin UI running on: http://localhost:5001/admin');
     console.log('--------------------------------------------------');
+    
+    console.log('Starting JobQueueService...');
+    await app.get(JobQueueService).start();
+    console.log('JobQueueService active and listening for background tasks.');
   })
   .catch((err) => {
     console.error('Error bootstrapping Vendure server:', err);
     process.exit(1);
   });
+
